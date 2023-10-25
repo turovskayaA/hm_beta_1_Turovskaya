ef69d7b1ffe77433ff31138dfc105967f9befe9e
anastasia turovskaya <turovskaya-nastya@mail.ru>
2023-10-25 20:53:22 +0200

    Add docstring and function def main()

--- src/widget.py
+++ src/widget.py
@@ -1,6 +1,12 @@
+from masks import card_number, account_number
 
 
-def type_card_and_account_number(type_and_number):
+def type_card_and_account_number(type_and_number: str) -> str:
+    """
+    Функция маскировки типа карта/счета с номером.
+    :param type_and_number: Номер и тип(карта/счет) для маскирования
+    :return: Маскированный по правилу тип и номер
+    """
     split_number = type_and_number.split()
     if "счет" in split_number:
         number = split_number[-1]
@@ -14,5 +20,9 @@ def type_card_and_account_number(type_and_number):
         return " ".join(name_and_masks)
 
 
-user_type_and_number = str(input())
-print(type_card_and_account_number(user_type_and_number))
+def main():
+    user_type_and_number = str(input()).lower()
+    print(type_card_and_account_number(user_type_and_number))
+
+if __name__ == "__main__":
+    main()
