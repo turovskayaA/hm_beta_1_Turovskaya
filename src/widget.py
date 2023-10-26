efcd89a388485ed3a6d79fd977844a3a2c865ef9
anastasia turovskaya <turovskaya-nastya@mail.ru>
2023-10-26 21:07:52 +0200

    Correction function

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,3 @@
-import maya
-from typing import Any
 from masks import card_number, account_number
 
 
@@ -10,30 +8,30 @@ def type_card_and_account_number(type_and_number: str) -> str:
     :return: Маскированный по правилу тип и номер
     """
     split_number = type_and_number.split()
-    if "счет" in split_number:
+    if "Счет" in split_number:
         number = split_number[-1]
         masks_number = account_number(number)
         name_and_masks = split_number[0], masks_number
         return " ".join(name_and_masks)
     else:
         number = split_number[-1]
+        name_card = " ".join(split_number[:-1])
         masks_number = card_number(number)
-        name_and_masks = split_number[0], masks_number
+        name_and_masks = name_card, masks_number
         return " ".join(name_and_masks)
 
 
-def datatime(data: str) -> Any:
+def datatime(data: str) -> str:
     """
     Функция преобразовании времени и даты
     :param data: Время и дату
     :return: Дату
     """
-    data_and_time = maya.parse(data).datetime()
-    return data_and_time.date()
+    return f'{data[8:10]}.{data[5:7]}.{data[0:4]}'
 
 
 def main():
-    user_type_and_number = str(input("Введите тип карты/счета и ее номер:")).lower()
+    user_type_and_number = str(input("Введите тип карты/счета и ее номер:")).title()
     print(type_card_and_account_number(user_type_and_number))
 
     user_datatime = "2018-07-11T02:26:18.671407"
