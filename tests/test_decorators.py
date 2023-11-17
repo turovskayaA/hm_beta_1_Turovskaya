5e4f67584e57ddab846e725c1a88a71d8523d64c
anastasia turovskaya <turovskaya-nastya@mail.ru>
2023-11-17 12:55:46 +0200

    Function correction (PEP8)

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -7,8 +7,7 @@ from src.decorators import log
 def test_function():
     def my_function(x, y):
         return x / y
-    date_time = datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')
+
+    date_time = datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")
     assert my_function(100, 1) == f"{date_time} my_function - ok"
     assert my_function(100, 0) == f"{date_time} my_function Error: division by zero. Inputs: (100, 0)"
-
-
