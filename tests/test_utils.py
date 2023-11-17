f0c5ec83fe954d39c34d1a317ac622a3a429a185
anastasia turovskaya <turovskaya-nastya@mail.ru>
2023-11-17 12:55:46 +0200

    Function correction (PEP8)

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,69 +6,49 @@ from data.settings import TEST_OPERATIONS_PATH, TEST_NOT_FOUND_OPERATIONS_PATH, 
 
 @pytest.fixture()
 def test_transactions():
-    return [{
-        "id": 441945886,
-        "state": "EXECUTED",
-        "date": "2019-08-26T10:50:58.294041",
-        "operationAmount": {
-            "amount": "31957.58",
-            "currency": {
-                "name": "руб.",
-                "code": "RUB"
-            }
+    return [
+        {
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "2019-08-26T10:50:58.294041",
+            "operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", "code": "RUB"}},
+            "description": "Перевод организации",
+            "from": "Maestro 1596837868705199",
+            "to": "Счет 64686473678894779589",
         },
-        "description": "Перевод организации",
-        "from": "Maestro 1596837868705199",
-        "to": "Счет 64686473678894779589"
-    },
         {
             "id": 939719570,
             "state": "EXECUTED",
             "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {
-                "amount": "9824.07",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
+            "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
             "description": "Перевод организации",
             "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702"
-        }]
+            "to": "Счет 11776614605963066702",
+        },
+    ]
 
 
 def test_transaction():
-    assert transaction(TEST_OPERATIONS_PATH) == [{
-        "id": 441945886,
-        "state": "EXECUTED",
-        "date": "2019-08-26T10:50:58.294041",
-        "operationAmount": {
-            "amount": "31957.58",
-            "currency": {
-                "name": "руб.",
-                "code": "RUB"
-            }
+    assert transaction(TEST_OPERATIONS_PATH) == [
+        {
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "2019-08-26T10:50:58.294041",
+            "operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", "code": "RUB"}},
+            "description": "Перевод организации",
+            "from": "Maestro 1596837868705199",
+            "to": "Счет 64686473678894779589",
         },
-        "description": "Перевод организации",
-        "from": "Maestro 1596837868705199",
-        "to": "Счет 64686473678894779589"
-    },
         {
             "id": 939719570,
             "state": "EXECUTED",
             "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {
-                "amount": "9824.07",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
+            "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
             "description": "Перевод организации",
             "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702"
-        }]
+            "to": "Счет 11776614605963066702",
+        },
+    ]
 
 
 def test_not_found_transaction():
@@ -80,7 +60,7 @@ def test_transaction_empty():
 
 
 def test_transaction_rub(test_transactions):
-    assert transaction_rub(test_transactions[0]) == '31957.58'
+    assert transaction_rub(test_transactions[0]) == "31957.58"
 
 
 def test_transaction_usd(test_transactions):
